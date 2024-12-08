53ce9d612a3ac40158de5e4054a7451a88c78f3f
Shanka Visal <shankavisal@gmail.com>
2024-12-08 23:44:54 +0530

    update

--- src/api/vehicleApi.jsx
+++ src/api/vehicleApi.jsx
@@ -1,53 +1,27 @@
 import axios from 'axios';
 
 export const getVehicles = async () => {
-  try {
-    const response = await axios.get('/api/vehicles');
-    console.log('Vehicles Response:', response.data);
-    return response.data.list;
-  } catch (error) {
-    console.error('Error fetching vehicles:', error);
-  }
+  const response = await axios.get('/api/vehicles');
+  return response.data.list;
 };
 
 export const createVehicle = async (vehicleData) => {
-  try {
-    const token = localStorage.getItem('token');
-    console.log('Token:', token);
-    const response = await axios.post('/api/vehicles', vehicleData, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-    console.log('Create Vehicle Response:', response.data);
-    return response.data;
-  } catch (error) {
-    console.error('Error creating vehicle:', error);
-  }
+  const response = await axios.post('/api/vehicles', vehicleData, {
+    headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+  });
+  return response.data;
 };
 
 export const updateVehicle = async (vehicleNumber, updatedData) => {
-  try {
-    const token = localStorage.getItem('token');
-    console.log('Token:', token);
-    const response = await axios.put(`/api/vehicles/${vehicleNumber}`, updatedData, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-    console.log('Update Vehicle Response:', response.data);
-    return response.data;
-  } catch (error) {
-    console.error('Error updating vehicle:', error);
-  }
+  const response = await axios.put(`/api/vehicles/${vehicleNumber}`, updatedData, {
+    headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+  });
+  return response.data;
 };
 
 export const deleteVehicle = async (vehicleNumber) => {
-  try {
-    const token = localStorage.getItem('token');
-    console.log('Token:', token);
-    const response = await axios.delete(`/api/vehicles/${vehicleNumber}`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-    console.log('Delete Vehicle Response:', response.data);
-    return response.data;
-  } catch (error) {
-    console.error('Error deleting vehicle:', error);
-  }
+  const response = await axios.delete(`/api/vehicles/${vehicleNumber}`, {
+    headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+  });
+  return response.data;
 };
